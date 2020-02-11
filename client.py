74ed5084635130c9e3820170b3277bf40494b23c
DInis <cruzdinis@ua.pt>
2020-02-11 11:02:14 +0000

    Primeira pratica (Dinis)

--- client.py
+++ client.py
@@ -1,10 +1,12 @@
 # Echo client program
 import socket
+import json
 
 HOST = 'localhost'      # Address of the host running the server  
 PORT = 5000             # The same port as used by the server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall('Hello, world'.encode('utf-8'))
+    x = json.dumps({"op":"register","user":"cd1"})
+    s.sendall(x)
     data = s.recv(1024)
 print(data.decode())
--- server.py
+++ server.py
@@ -1,18 +1,38 @@
 # Echo server program
 import socket
+import json
+import selectors
 
 HOST = ''                 # Symbolic name meaning all available interfaces
 PORT = 5000               # Arbitrary non-privileged port
+sel = selectors.DefaultSelector()
+clients = {}
+
+def accept(sock, mask):
+    conn, addr = sock.accept()  # Should be ready
+    print('accepted', conn, 'from', addr)
+    conn.setblocking(False)
+    sel.register(conn, selectors.EVENT_READ, read)
+
+def read(conn, mask):
+    data = conn.recv(1000)  # Should be ready
+    msg = json.loads(data)
+    if msg['op'] == 'register':
+        clients[msg['user']] = conn  # Hope it won't block
+    elif msg['op'] == 'msg':
+        print('closing', conn)
+    else:
+        sel.unregister(conn)
+        conn.close()
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen(1)
+    sock.setblocking(False)
     conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data: break
-            print("RECV: " + data.decode())
-            reply = "ECHO " + data.decode() 
-            print("SEND: " + reply)
-            conn.sendall(reply.encode('utf-8'))
+    sel.register(conn, selectors.EVENT_READ, accept)
+    while True:
+        events = sel.select()
+        for key, mask in events:
+            callback = key.data
+            callback(key.fileobj, mask)
