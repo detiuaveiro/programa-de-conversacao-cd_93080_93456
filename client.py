bb10d9042776e6697b8b61f94e1f0176d6a83854
DInis <cruzdinis@ua.pt>
2020-02-18 10:48:05 +0000

    Concluida a parte com sockets e selectors

--- client.py
+++ client.py
@@ -1,12 +1,54 @@
 # Echo client program
 import socket
+import select
 import json
+import selectors
+import sys
+import time
 
 HOST = 'localhost'      # Address of the host running the server  
-PORT = 5000             # The same port as used by the server
+PORT = 7000            # The same port as used by the server
+sel = selectors.DefaultSelector()
+name = input("Nome do cliente: ")
+
+def read(conn, mask):
+    data = conn.recv(1000)  # Should be ready
+    msg = json.loads(data)
+    print(time.ctime(msg["ts"]),msg["user"]," said: ",msg["data"])
+
+def got_keyboard_data(arg1, mask):
+    msg = arg1.readline()
+    msg = msg.rstrip()
+    if msg == 'quit':
+        z = {
+            "op":"unregister",
+            "user":name,
+            "ts":time.time()
+        }
+        w = json.dumps(z).encode('utf-8')
+        s.sendall(w)
+        s.close()
+        exit()
+    else:
+        z = {
+            "op":"msg",
+            "data":msg,
+            "user":name,
+            "ts":time.time()
+        }
+        w = json.dumps(z).encode('utf-8')
+        s.sendall(w)
+    
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    x = json.dumps({"op":"register","user":"cd1"})
-    s.sendall(x)
-    data = s.recv(1024)
-print(data.decode())
+    strjson = json.dumps({"op":"register","user":name})
+    s.sendall(strjson.encode("utf-8"))
+    sel.register(s, selectors.EVENT_READ, read)
+    sel.register(sys.stdin, selectors.EVENT_READ, got_keyboard_data)
+    while True:
+        print("Digite a sua mensagem: ")
+        events = sel.select()
+        for key, mask in events:
+            callback = key.data
+            callback(key.fileobj, mask)
--- server.py
+++ server.py
@@ -1,38 +1,46 @@
 # Echo server program
 import socket
+import select
 import json
 import selectors
+import time
 
 HOST = ''                 # Symbolic name meaning all available interfaces
-PORT = 5000               # Arbitrary non-privileged port
+PORT = 7000             # Arbitrary non-privileged port
 sel = selectors.DefaultSelector()
 clients = {}
 
 def accept(sock, mask):
     conn, addr = sock.accept()  # Should be ready
     print('accepted', conn, 'from', addr)
-    conn.setblocking(False)
+    data = conn.recv(1024)
+    user = json.loads(data)
+    clients[user["user"]]=conn
     sel.register(conn, selectors.EVENT_READ, read)
 
 def read(conn, mask):
     data = conn.recv(1000)  # Should be ready
     msg = json.loads(data)
-    if msg['op'] == 'register':
-        clients[msg['user']] = conn  # Hope it won't block
-    elif msg['op'] == 'msg':
-        print('closing', conn)
+    if msg["op"] == "register":
+        clients[msg["user"]] = conn  # Hope it won't block
+    elif msg["op"] == "msg":
+        for key in clients:
+            if key != msg["user"]:
+                clients[key].sendall(data)
     else:
         sel.unregister(conn)
+        print(time.ctime(msg["ts"]),": Disconecting...")
+        del clients[msg["user"]]
         conn.close()
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
-    s.listen(1)
-    sock.setblocking(False)
-    conn, addr = s.accept()
-    sel.register(conn, selectors.EVENT_READ, accept)
+    s.listen(100)
+    s.setblocking(False)
+    sel.register(s, selectors.EVENT_READ, accept)
     while True:
         events = sel.select()
         for key, mask in events:
             callback = key.data
             callback(key.fileobj, mask)
+
