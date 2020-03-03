619eef6e2b72a45d0206dc181bcda7164e4dae23
DInis <cruzdinis@ua.pt>
2020-03-03 17:11:12 +0000

    Atualizado com os delimitadores, não da maneira mais segura de acordo com o professor nuno lau, assim que possivel vamos alterar para uma leitura byte a byte

--- client.py
+++ client.py
@@ -7,14 +7,15 @@ import sys
 import time
 
 HOST = 'localhost'      # Address of the host running the server  
-PORT = 3700            # The same port as used by the server
+PORT = 3725       # The same port as used by the server
 sel = selectors.DefaultSelector()
 name = input("Nome do cliente: ")
 channel=input("Nome do canal: ")
 
 def read(conn, mask):
-    data = conn.recv(1000)  # Should be ready
-    msg = json.loads(data)
+    data = conn.recv(1024)  # Should be ready
+    index = data.decode('utf-8').find("\r\m")
+    msg = json.loads(data.decode('utf-8')[:index])
     print(time.ctime(msg["ts"]),msg["user"]," said: ",msg["data"])
 
 def got_keyboard_data(arg1, mask):
@@ -26,7 +27,7 @@ def got_keyboard_data(arg1, mask):
             "user":name,
             "ts":time.time()
         }
-        w = json.dumps(z).encode('utf-8')
+        w = (json.dumps(z)+"\r\m").encode('utf-8')
         s.sendall(w)
         s.close()
         exit()
@@ -38,14 +39,17 @@ def got_keyboard_data(arg1, mask):
             "channel":channel,
             "ts":time.time()
         }
-        w = json.dumps(z).encode('utf-8')
+        w = (json.dumps(z)+"\r\m").encode('utf-8')
         s.sendall(w)
     
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    strjson = json.dumps({"op":"register","user":name,"channel":channel})
-    s.sendall(strjson.encode("utf-8"))
+    print("Connectando...")
+    strjson = (json.dumps({"op":"register","user":name,"channel":channel})+"\r\m").encode('utf-8')
+    s.sendall(strjson)
+    strjson = (json.dumps({"op":"msg","data":"ole","user":name,"channel":channel,"ts":time.time()})+"\r\m").encode('utf-8')
+    s.sendall(strjson)
     sel.register(s, selectors.EVENT_READ, read)
     sel.register(sys.stdin, selectors.EVENT_READ, got_keyboard_data)
     while True:
--- server.py
+++ server.py
@@ -6,7 +6,7 @@ import selectors
 import time
 
 HOST = ''                 # Symbolic name meaning all available interfaces
-PORT = 3700             # Arbitrary non-privileged port
+PORT = 3725            # Arbitrary non-privileged port
 sel = selectors.DefaultSelector()
 clients = {}
 channels = {}
@@ -14,27 +14,31 @@ channels = {}
 def accept(sock, mask):
     conn, addr = sock.accept()  # Should be ready
     print('accepted', conn, 'from', addr)
-    data = conn.recv(1024)
-    user = json.loads(data)
-    clients[user["user"]]=conn
-    channels[user["user"]]=user["channel"]
     sel.register(conn, selectors.EVENT_READ, read)
 
 def read(conn, mask):
-    data = conn.recv(1000)  # Should be ready
-    msg = json.loads(data)
-    if msg["op"] == "register":
-        clients[msg["user"]] = conn  # Hope it won't block
-    elif msg["op"] == "msg":
-        for key in channels:
-            if channels[key] == msg["channel"] and key != msg["user"]:
-
-                clients[key].sendall(data)
-    else:
-        sel.unregister(conn)
-        print(time.ctime(msg["ts"]),": Disconecting...")
-        del clients[msg["user"]]
-        conn.close()
+    data = conn.recv(4096)  # Should be ready
+    a_data=data.decode('utf-8')
+    index=a_data.find("\r\m")
+    while (index !=-1 and a_data[:index] !=""):
+        this_data = a_data[:index]
+        a_data = a_data[index:]
+        this_data =json.loads(this_data)
+        print('echoing', repr(this_data), 'to', conn)
+        if this_data["op"] == "register":
+            print("resgistering")
+            clients[this_data["user"]] = conn  # Hope it won't block
+            channels[this_data["user"]] = this_data["channel"]
+        elif this_data["op"] == "msg":
+            for key in clients:
+                if channels[key] == this_data["channel"] and key != this_data["user"]:
+                    clients[key].sendall((json.dumps(this_data)+"\r\m").encode('utf-8'))
+        else:
+            sel.unregister(conn)
+            print(time.ctime(this_data["ts"]),": Disconecting...")
+            del clients[this_data["user"]]
+            conn.close()
+        index = a_data.find("\r\m")
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
